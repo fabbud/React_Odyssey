a0c23ac236e88aa7ea5712f5df72edcf84dcc56b
fabbud <fabbud@gmail.com>
2020-06-22 13:18:00 +0100

    abbud_fernanda_quest040

--- back/routes/auth/auth.js
+++ back/routes/auth/auth.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const connection = require('../../helpers/db');
 
 router.post('/signup', function (req, res, next) {
-  res.send('I am in POST signup');
+  const formData = req.body;
+
+  return connection.query(
+    'INSERT INTO users SET ?',
+    [formData],
+    (err, results) => {
+      if (err) {
+        res.status(500).send('error saving new user').end();
+      }
+      res.end();
+    },
+  );
 });
 
 module.exports = router;
