f176d81d838e7fe587d87bb1a319fd691d15453f
fabbud <fabbud@gmail.com>
2020-06-25 18:09:38 +0100

    backend commit quest060

--- back/app.js
+++ back/app.js
@@ -8,20 +8,24 @@ const app = express();
 const authRouter = require('./routes/auth/auth');
 const connection = require('./helpers/db.js');
 
+connection.connect((err) => {
+  if (err) throw err;
+  console.log('Connected to the database successfully');
+});
+
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
 app.use('/auth', authRouter); //where authRouter is imported
+app.get('/', (req, res) => {
+  res.send('youhou');
+});
 /// in case path is not found, return the 'Not Found' 404 code
 app.use(function (req, res, next) {
   let err = new Error('Not Found');
   err.status = 404;
   next(err);
-});
-
-app.get('/', (req, res) => {
-  res.send('youhou');
 });
 
 // launch the node server
